a6d26e5b93a3ebf3b148a742e4919cc84920384b
icetabetai0403 <spngbb.ptrc@gmail.com>
2024-07-14 00:39:42 +0900

    レスポンシブ5回目コミット

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,23 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var carousel = document.getElementById("recommendedStoresCarousel");
-    var carouselInstance = new bootstrap.Carousel(carousel, {
-        interval: 5000,
-        wrap: true,
-        keyboard: true,
-    });
+    var carouselPC = document.getElementById("recommendedStoresCarousel");
+    var carouselMobile = document.getElementById(
+        "recommendedStoresCarouselMobile"
+    );
 
-    var dots = document.querySelectorAll(".carousel-custom-indicators .dot");
+    var carouselInstancePC = carouselPC
+        ? new bootstrap.Carousel(carouselPC, {
+              interval: 5000,
+              wrap: true,
+              keyboard: true,
+          })
+        : null;
 
-    carousel.addEventListener("slide.bs.carousel", function (e) {
-        dots.forEach(function (dot) {
-            dot.classList.remove("active");
+    var carouselInstanceMobile = carouselMobile
+        ? new bootstrap.Carousel(carouselMobile, {
+              interval: 5000,
+              wrap: true,
+              keyboard: true,
+          })
+        : null;
+
+    var dotsPC = document.querySelectorAll(
+        ".carousel-custom-indicators .d-none.d-lg-block .dot"
+    );
+    var dotsMobile = document.querySelectorAll(
+        ".carousel-custom-indicators .d-lg-none .dot"
+    );
+
+    function updateDots(dots, activeIndex) {
+        dots.forEach(function (dot, index) {
+            if (index === activeIndex) {
+                dot.classList.add("active");
+            } else {
+                dot.classList.remove("active");
+            }
         });
-        dots[e.to].classList.add("active");
+    }
+
+    if (carouselPC) {
+        carouselPC.addEventListener("slide.bs.carousel", function (e) {
+            updateDots(dotsPC, e.to);
+        });
+    }
+
+    if (carouselMobile) {
+        carouselMobile.addEventListener("slide.bs.carousel", function (e) {
+            updateDots(dotsMobile, e.to);
+        });
+    }
+
+    dotsPC.forEach(function (dot, index) {
+        dot.addEventListener("click", function () {
+            if (carouselInstancePC) {
+                carouselInstancePC.to(index);
+            }
+        });
     });
 
-    dots.forEach(function (dot, index) {
+    dotsMobile.forEach(function (dot, index) {
         dot.addEventListener("click", function () {
-            carouselInstance.to(index);
+            if (carouselInstanceMobile) {
+                carouselInstanceMobile.to(index);
+            }
         });
     });
 });
