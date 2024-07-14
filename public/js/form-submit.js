fc7ef63fa942870eb208b79609b6fdabe53b09ef
icetabetai0403 <spngbb.ptrc@gmail.com>
2024-07-14 22:25:37 +0900

    ボタンの挙動調整

--- public/js/form-submit.js
+++ public/js/form-submit.js
@@ -3,11 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
     forms.forEach(function (form) {
         form.addEventListener("submit", function (e) {
             var submitButton = form.querySelector(".nagoyameshi-submit-button");
-            if (submitButton && !submitButton.disabled) {
-                submitButton.disabled = true;
-                submitButton.textContent = "送信中...";
-            } else {
-                e.preventDefault(); // フォームの送信を阻止
+            if (submitButton) {
+                if (!submitButton.disabled) {
+                    submitButton.disabled = true;
+                } else {
+                    e.preventDefault(); // フォームの送信を阻止
+                }
             }
         });
     });
